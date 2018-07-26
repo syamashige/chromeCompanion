5278b7f29db8621eacc8d3aba2b9b9e26e50b59c
Sarah <yamashige.sarah@gmail.com>
2018-07-25 22:32:04 -1000

    Cursor works with presets, but doesn't work with the url

---
+++ content.js
@@ -0,0 +1,22 @@
+console.log("sanity check");
+
+// document.body.style.backgroundColor = "yellow";
+
+// document.body.style.cursor = "url('kawaii-donut.png'), auto";
+document.body.style.cursor = "https://www.dropbox.com/home/DevLeague?preview=kawaii-donut.png";
+
+// document.body.style.backgroundImage = "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJ9y9PElf2dEix3ZXQwlwHx-XVihjNZXqmQUV-UiP8af6I8YyC')";
+
+// document.body.style.backgroundSize = "contain";
+
+// document.getElementsByTagName("p").style.cursor = "url('/kawaii_donut.png')";
+
+// document.body.addEventListener('onmouseover', setCursor);
+
+// function setCursor() {
+//     console.log('change cursor');
+//     document.body.style.cursor = "url('/kawaii-donut.png')"
+// }
+
+// let getBody = document.getElementsByTagName("body");
+// getBody.style.cursor = "https://www.dropbox.com/home/DevLeague?preview=kawaii-donut.png";
